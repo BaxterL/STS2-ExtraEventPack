41d21f2bb3f342dd10dd364f1d30e54d176d22d2
BaxterL <2954839300@qq.com>
2026-06-03 17:38:23 +0800

    更新了一个事件

---
+++ ExtraEventsCode/Events/InfiniteGold.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Events;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Runs;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace ExtraEvents.ExtraEventsCode.Events;
+
+[RegisterSharedEvent]
+public sealed class InfiniteGold : ModEventTemplate
+{
+    public override bool IsShared => true;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new GoldVar(998),
+        new GoldVar("PartialGold", 333),
+    ];
+
+    public override EventAssetProfile AssetProfile => new(
+        InitialPortraitPath: Res.EventPortrait<InfiniteGold>()
+    );
+
+    public override bool IsAllowed(IRunState runState) => true;
+
+    protected override IReadOnlyList<EventOption> GenerateInitialOptions() =>
+    [
+        new EventOption(this, TakeAll, InitialOptionKey("TAKE_ALL")),
+        new EventOption(this, TakeSome, InitialOptionKey("TAKE_SOME")),
+    ];
+
+    private async Task TakeAll()
+    {
+        await PlayerCmd.GainGold(DynamicVars.Gold.BaseValue, Owner!);
+        var curse = Owner!.RunState.CreateCard<Normality>(Owner);
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.Add(curse, PileType.Deck), 2f);
+        SetEventFinished(L10NLookup($"{Id.Entry}.pages.TAKE_ALL.description"));
+    }
+
+    private async Task TakeSome()
+    {
+        await PlayerCmd.GainGold(DynamicVars["PartialGold"].BaseValue, Owner!);
+        var curse = Owner!.RunState.CreateCard<Greed>(Owner);
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.Add(curse, PileType.Deck), 2f);
+        SetEventFinished(L10NLookup($"{Id.Entry}.pages.TAKE_SOME.description"));
+    }
+}
--- ExtraEventsCode/Res.cs
+++ ExtraEventsCode/Res.cs
@@ -1,28 +1,38 @@
+using System.Text.RegularExpressions;
 using Godot;
 
 namespace ExtraEvents.ExtraEventsCode;
 
 public static class Res
 {
-    public static string EventPortrait<T>() => ImagePath("events", typeof(T).Name);
-    public static string EventPortrait(string className) => ImagePath("events", className);
+    public static string EventPortrait<T>() => EventPortrait(typeof(T).Name);
+    public static string EventPortrait(string className) => ImagePath("events", "event", className);
 
-    public static string CardPortrait<T>() => ImagePath("cards", typeof(T).Name);
-    public static string CardPortrait(string className) => ImagePath("cards", className);
+    public static string CardPortrait<T>() => CardPortrait(typeof(T).Name);
+    public static string CardPortrait(string className) => ImagePath("cards", "card", className);
 
-    public static string RelicIcon<T>() => ImagePath("relics", typeof(T).Name);
-    public static string RelicIcon(string className) => ImagePath("relics", className);
+    public static string RelicIcon<T>() => RelicIcon(typeof(T).Name);
+    public static string RelicIcon(string className) => ImagePath("relics", "relic", className);
 
-    public static string RelicOutline<T>() => ImagePath("relics", typeof(T).Name);
-    public static string RelicOutline(string className) => ImagePath("relics", className);
+    public static string RelicOutline<T>() => RelicOutline(typeof(T).Name);
+    public static string RelicOutline(string className) => ImagePath("relics", "relic", className);
 
-    public static string RelicBigIcon<T>() => ImagePath("relics", typeof(T).Name);
-    public static string RelicBigIcon(string className) => ImagePath("relics", className);
+    public static string RelicBigIcon<T>() => RelicBigIcon(typeof(T).Name);
+    public static string RelicBigIcon(string className) => ImagePath("relics", "relic", className);
 
-    private static string ImagePath(string category, string className)
+    private static string ImagePath(string categoryFolder, string categorySingular, string className)
     {
-        var specific = $"{Entry.ResPath}/images/{category}/{className}.png";
-        var fallback = $"{Entry.ResPath}/images/{category}/common.png";
+        var modSnake = PascalToSnake(Entry.ModId);
+        var classSnake = PascalToSnake(className);
+        var filename = $"{modSnake}_{categorySingular}_{classSnake}.png";
+        var specific = $"{Entry.ResPath}/images/{categoryFolder}/{filename}";
+        var fallback = $"{Entry.ResPath}/images/{categoryFolder}/common.png";
         return ResourceLoader.Exists(specific) ? specific : fallback;
+    }
+
+    private static string PascalToSnake(string pascal)
+    {
+        var result = Regex.Replace(pascal, "([a-z])([A-Z])", "$1_$2");
+        return result.ToLowerInvariant();
     }
 }
